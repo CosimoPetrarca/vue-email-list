5701c0d2f4458d56850971c609ca5dfbf85884b2
P4anix <pancok_1@yahoo.it>
2023-03-21 17:27:13 +0100

    done

--- js/main.js
+++ js/main.js
@@ -5,7 +5,17 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            emails: [], //array vuoto di mails
         }
+    },
+
+    created() {
+        for (let index = 0; index < 10; index++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {                
+                console.log(response);
+                this.emails.push(response.data.response)
+            })
+        }       
     }
 }).mount('#app')
